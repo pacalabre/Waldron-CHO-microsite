a6450b29bcad650856a50dba6df12008a718adfb
Paul <pacalabre@gmail.com>
2016-10-19 12:00:27 -0700

    added mock events, changed the spacing above the events sections

--- js/main.js
+++ js/main.js
@@ -4,6 +4,37 @@ $(function() {
     });
   });
 
+var eventData = [
+  {
+    "date": "2016-10-19",
+    "badge": false,
+    "title": "YEEZY",
+    "body":"<p>Ultra Light</p>",
+    "footer":"Key Arena"
+  },
+  {
+    "date": "2016-10-23",
+    "badge": false,
+    "title": "Bulls",
+    "body":"<p>Ultra Light</p>",
+    "footer":"Definitely not at Key Arena"
+  }
+];
+
 $(document).ready(function () {
-        $("#my-calendar").zabuto_calendar();
+        $("#my-calendar").zabuto_calendar({
+          today: true,
+          data: eventData,
+          modal: true
+        });
     })
+
+// $("#my-calendar").zabuto_calendar( { data: [{
+//   "date": "2016-10-19",
+//   "badge": true,
+//   "title": "YEEZY",
+//   "body":"<p>Ultra Light</p>",
+//   "footer":"Key Arena"
+// }] } );
+
+
