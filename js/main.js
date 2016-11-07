32b4707ff3465a7a2e27fa066be4288e2697611c
Paul <pacalabre@gmail.com>
2016-11-07 10:36:21 -0800

    added text and event image to the Seattle page

--- js/main.js
+++ js/main.js
@@ -4,19 +4,19 @@
 
 var eventData = [
   {
-    "date": "2016-10-19",
-    "badge": false,
-    "title": "CHO event",
-    "body":"<p></p>",
-    "footer":"Seattle"
-  },
-  {
-    "date": "2016-10-23",
-    "badge": false,
-    "title": "CHO Event",
-    "body":"<p></p>",
-    "footer":"Portland"
+    "date": "2016-11-30",
+    "badge": true,
+    "title": "The Purpose Effect",
+    "body":"<p>The Impact of Aligning Personal, Role, and Organizational Purpose</p>",
+    "footer":"701 5TH Avenue, Suite 6100 Columbia Center Seattle, WA"
   }
+  // ,{
+  //   "date": "2016-10-23",
+  //   "badge": false,
+  //   "title": "CHO Event",
+  //   "body":"<p></p>",
+  //   "footer":"Portland"
+  // }
 ];
 
 $(document).ready(function () {
